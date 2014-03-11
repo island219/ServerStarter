c719a05f0727f4eadc96417d13238bd8dcb69eb8
Joshua Baldwin <iam@joshuabaldwin.eu>
2014-03-11 20:07:13 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,10 @@
 while read line;
 	do
 		name=${line%%;*}; next=${line#*;}
-		host=${next%%;*}; next=${rest#*;}
-		post=${next%%;*};
-
+                host=${next%%;*}; next=${next#*;}
+                port=${next%%;*};
+		
+		echo -e $name $host $port
 		cd /athena/servers/$name/
 		./start.sh $host $port
 done < servers.txt
